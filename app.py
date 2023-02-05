cd1455d5747381515b00a0beb127d4badbd47875
chu thomas <chu_liang_han@hotmail.com>
2023-02-05 12:27:44 +0800

    updae

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, session, request, url_for
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.get('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    # return 'Hello World!'
+    test: dict = {"name": "gogogo"}
+    print(url_for("static", filename="style.css"))
+    print(url_for("hello_world"))
+    return render_template("index.html", test=test)
 
+@app.get('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.post('/contact/complete')
+def contact_complete():
+    if request.method == 'POST':
+        return redirect(url_for('contact_complete'))
+    return render_template('contact_complete.html')
 
 if __name__ == '__main__':
     app.run()
